81bedd7ac3ccf2578ed638e14c568a8556abef52
JM Arenas <jm.arenas@interleukin.co>
2022-10-10 15:57:40 +0800

    Initialized sessions create and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   namespace :v1 do
     resources :contacts
+    resources :sessions, only: [:create, :destroy]
   end
 
 end
